11b79867537128232e13ad6e970c87c5667d24dd
ryan-redfern <78377972+ryan-redfern@users.noreply.github.com>
2021-02-19 18:24:17 +0000

    Add files via upload

--- FormatNamesm.java
+++ FormatNamesm.java
@@ -9,14 +9,14 @@ import java.text.ParseException;
  * CSCU9T4 Java strings and files exercise.
  *
  */
-public class FilesInOut {
+public class FormatNamesm {
 
     public static void main(String[] args) throws ParseException, IOException { 
        String output; 
        String input;
        Boolean upper = false;
        if (!args[0].equals("-u")){
-            
+           
             input = args[0];
             output = args[1];  
        }
@@ -24,7 +24,10 @@ public class FilesInOut {
        upper = true;
        input = args[1];
        output = args[2];
+       
        }
+       
+       
        File fileOutput = new File(output);
        fileOutput.delete();
        try {
@@ -39,13 +42,23 @@ public class FilesInOut {
            }
         try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
             while (reader.ready()) {
-                
                 String inputString = reader.readLine();
                 String[] words = inputString.split(" ");
+                String text2;
                 
                 String date = words[words.length - 1];
                 String formattedDate = date.substring(0,2) + "/" + date.substring(2, 4) + "/" + date.substring(4, date.length());
-                String text2 = inputString.replace(date, formattedDate);
+                text2 = inputString.replace(date, formattedDate);
+                if (words.length == 4){
+                    String a = words[words.length - 3];
+                    a = a.toUpperCase();
+                    String finish = a + ".";
+                    text2 = inputString.replace(words[words.length - 3], finish);
+                    
+                    
+                            
+                    
+                }
                 if (upper = true){
                     text2 = text2.toUpperCase();  
                 }
